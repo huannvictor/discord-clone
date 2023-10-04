8b91662abf960dc7a0b475f0f37b41b3a39ccc3e
Huann Almeida <huannvictor@gmail.com>
2023-10-04 15:40:46 -0300

    UploadThing setup

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -5,9 +5,24 @@ import { useEffect, useState } from "react";
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
 
+import { FileUpload } from "@/components/file-upload";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
 const formSchema = z.object({
@@ -59,7 +74,21 @@ const InitialModal = () => {
           >
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                TODO: image upload
+                <FormField 
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload 
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
 
               <FormField 
