5fe14cb1b4178da32a73da08d0244a2b1f5dc92c
Huann Almeida <huannvictor@gmail.com>
2023-10-05 15:24:26 -0300

    Navigation sidebar

--- app/api/servers/route.ts
+++ app/api/servers/route.ts
@@ -2,7 +2,7 @@ import { MemberRole } from "@prisma/client"
 import { NextResponse } from "next/server"
 import { v4 as uuidv4 } from "uuid"
 
-import { currentProfile } from "@/lib/curret-profile"
+import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db"
 
 export async function POST(req: Request) {
