a572de16f2b55803efdb44b6596ab4508f0e37f9
Huann Almeida <huannvictor@gmail.com>
2023-10-08 20:46:56 -0300

    Delete Server Modal

---
+++ components/modals/leave-server-modal.tsx
@@ -0,0 +1,78 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle
+} from "@/components/ui/dialog";
+import { useModal } from "@/hooks/use-modal-store";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export const LeaveServerModal = () => {
+  const { isOpen, onClose, type, data } = useModal()
+  const [ isLoading, setIsLoading ] = useState(false)
+  const router = useRouter()
+
+  const isModalOpen = isOpen && type === 'leaveServer'
+  const { server } = data
+
+  const onConfirm = async () => {
+    try {
+      setIsLoading(true)
+      await axios.patch(`/api/servers/${server?.id}/leave`)
+      
+      onClose()
+      router.refresh()
+      router.push("/")
+    } catch (error) {
+      console.error("onConfirm", error)
+    }
+  }
+  return ( 
+    <Dialog 
+      open={isModalOpen}
+      onOpenChange={onClose}
+    >
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Leave Server
+          </DialogTitle>
+
+          <DialogDescription className="text-center text-zinc-500">
+            Are you sure you want to leave{" "}
+            <span className="font-semibold text-indigo-500">
+              {server?.name}
+            </span>?
+          </DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter className="bg-gray-100 px-6 py-4">
+          <div className="flex items-center justify-between w-full">
+            <Button
+              onClick={onClose}
+              disabled={isLoading}
+              variant="ghost"
+            >
+              Cancel
+            </Button>
+            
+            <Button
+              onClick={onConfirm}
+              disabled={isLoading}
+              variant="primary"
+            >
+              Confirm
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
