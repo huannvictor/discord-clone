8d49e341d381f1a4eaf5b050916af615a791c6cf
Huann Almeida <huannvictor@gmail.com>
2023-10-08 19:44:16 -0300

    Channel Creation

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -85,6 +85,7 @@ export const ServerHeader = ({
 
         {isModerator && (
           <DropdownMenuItem
+            onClick={() => onOpen('createChannel', { server })}
             className="px-3 py-2 text-sm cursor-pointer"
           >
             Create Channel
