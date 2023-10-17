d109e5aa616eac2d432e4a21759f8436fee2502a
Huann Almeida <huannvictor@gmail.com>
2023-10-17 11:24:03 -0300

    Delete Message

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -14,6 +14,7 @@ import { Button } from "@/components/ui/button"
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { UserAvatar } from "@/components/user-avatar"
+import { useModal } from "@/hooks/use-modal-store"
 import axios from "axios"
 import { Edit, File, ShieldCheck, Trash } from "lucide-react"
 import Image from "next/image"
@@ -58,7 +59,7 @@ export const ChatItem = ({
   socketQuery
 }: ChatItemProps) => {
   const [isEditing, setIsEditing] = useState(false)
-  const [isDeleting, setIsDeleting] = useState(false)
+  const { onOpen } = useModal()
 
   useEffect(() => {
     const handleKeyDown = (event: any) => {
@@ -226,7 +227,13 @@ export const ChatItem = ({
             </ActionTooltip>
           )}
           <ActionTooltip label="Delete">
-            <Trash className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition" />
+            <Trash
+              onClick={() => onOpen('deleteMessage', {
+                apiUrl: `${socketUrl}/${id}`,
+                query: socketQuery
+              })}
+              className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition"
+            />
           </ActionTooltip>
         </div>
       )}
